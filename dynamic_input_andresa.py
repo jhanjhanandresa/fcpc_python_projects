cd306148e347c4effc9c32db12249674465a8065
jhanjhanandresa <45374855+jhanjhanandresa@users.noreply.github.com>
2018-11-27 07:53:54 +0800

    Create dynamic_input_andresa.py

---
+++ dynamic_input_andresa.py
@@ -0,0 +1,33 @@
+Using the pattern of Caesar Cipher, where A=B, B=C, C=D ....
+
+Write a python code capable of conversion, with dynamic input.
+
+Point System:
+
+10 pts for input
+20 pts for algorithm
+
+Total of: 30 pts
+This will serve as grade for Quiz. (LA)
+Gawa mo
+Naisumite
+
+dynamic_input_ANDRESA.py
+Text
+
+gene_decoding_ANDRESA.py
+Text
+Challenge: Caesar Cipher
+Minamarkahan na tapos na…
+#accept input form user
+name = input("Enter your name:")
+print("Your name is: " + name)
+
+#accept numerical input -- int()
+std = int(input("Enter First Number: "))
+snum = int(input("Enter Second Number: "))
+
+to_int_value = int("10")
+
+print(fnum + snum)
+print(to_int_value ** 2)
