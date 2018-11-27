abef6749aeed3dda7a17c092165f40c561082936
jhanjhanandresa <45374855+jhanjhanandresa@users.noreply.github.com>
2018-11-27 11:17:52 +0800

    test commit

---
+++ activity_A_andresa.py
@@ -0,0 +1,15 @@
+for x in range(1, 17 ,3):
+    print(x,end="\t")
+
+print("\n")
+
+for y in range(100,75,-7):
+    print(y,end="\t")
+
+print("\n")
+
+for z in  range (1,5):
+    if z%3==0:
+        print("1",end="\t")
+    else:
+        print("o",end="\t")
